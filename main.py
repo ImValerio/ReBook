1924df80daa2243b72570777069599a166e7562a
Valerio <valerio.valletta@icloud.com>
2022-12-05 23:04:03 +0100

    add word variations implementing ngram

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
+from whoosh.analysis import StemmingAnalyzer, NgramFilter, StandardAnalyzer, NgramTokenizer
 from whoosh.index import create_in, open_dir
 from whoosh.fields import *
 import os, os.path
 import csv
 
 from whoosh.qparser import QueryParser
+my_analyzer = StandardAnalyzer()
 
-# schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True))
-# if not os.path.exists("indexdir"):
-#     os.mkdir("indexdir")
-# ix = create_in("indexdir", schema)
-# writer = ix.writer()
-#
-#
-#
-# with open('dataset/Reviews.csv') as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     line_count = 0
-#     for row in csv_reader:
-#         if line_count > 100:
-#             break
-#         if line_count == 0:
-#             print(f'Column names are {", ".join(row)}')
-#             line_count += 1
-#         else:
-#             writer.add_document(title=row[8], path=row[0], content=row[9])
-#             line_count += 1
-#     print(f'Processed {line_count} lines.')
-#     writer.commit()
+schema = Schema(title=TEXT(analyzer=my_analyzer,stored=True), path=ID(stored=True), content=NGRAMWORDS(minsize=0,maxsize=4, stored=True))
+#schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True))
+if not os.path.exists("indexdir"):
+    os.mkdir("indexdir")
+ix = create_in("indexdir", schema)
+writer = ix.writer()
 
-#Search in documents
-ix = open_dir("indexdir")
-searcher = ix.searcher()
-parser = QueryParser("content", ix.schema)
-print(ix.schema)
-query =parser.parse(u"good")
-results = searcher.search(query)
 
-for res in results:
-    print(res["content"])
+with open('dataset/Reviews.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count > 100:
+            break
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            writer.add_document(title=row[8], path=row[0], content=row[9])
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+    writer.commit()
+
+
--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from whoosh.analysis import NgramTokenizer, StandardAnalyzer, NgramFilter
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
 
@@ -15,12 +16,13 @@ class SearchText(BaseModel):
 @app.post("/search")
 def read_item(search: SearchText):
     ix = open_dir("../indexdir")
-
+    from whoosh import query
     searcher = ix.searcher()
-    parser = QueryParser("content", ix.schema)
+    parser = QueryParser("content", ix.schema, termclass=query.Variations)
     query = parser.parse(search.text)
 
     results = searcher.search(query)
+    print([token for token in query.all_tokens()])
 
     data = [{"id": res["path"], "title": res["title"], "content": res["content"]} for res in results]
 
