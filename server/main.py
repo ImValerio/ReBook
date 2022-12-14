bb4ab62d0ff9a13e6f88a6b96c8062ef8486dfb7
Valerio <valerio.valletta@icloud.com>
2022-12-14 23:03:47 +0100

    basic weighting

--- server/main.py
+++ server/main.py
@@ -8,44 +8,47 @@ from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
 from whoosh.query import *
 
-
 app = FastAPI()
 
 
 class SearchText(BaseModel):
     text: Union[str, None] = None
-# def custom_scoring(searcher, fieldname, text, matcher):
-#     bm25 = scoring.BM25F().scorer(searcher, fieldname, text).score(matcher)
-#     return  bm25 + score
 
-@app.post("/search")
+
+def pos_score_fn(searcher, fieldname, text, matcher):
+    poses = matcher.value_as("positions")
+    print([field for field in searcher.all_stored_fields()])
+    return 1.0 / (poses[0] + 1)
+
+
+@ app.post("/search")
 def read_item(search: SearchText):
     ix = open_dir("../indexdir")
-    scoring.BM25F(B=0.75, content_B=1.0, K1=1.5)
-    # scoring.FunctionWeighting()
+    # k1: importanza frequenza di un termine; b: importanza lunghezza del documento
+    # scoring.BM25F(B=-10.0, content_B=1.0, K1=1)
 
+    pos_weighting = scoring.FunctionWeighting(pos_score_fn)
     from whoosh import query
-    searcher = ix.searcher()
+    searcher = ix.searcher(weighting=pos_weighting)
     parser = QueryParser("content", ix.schema, termclass=query.Variations)
     query = parser.parse(search.text)
 
     results = searcher.search(query)
-    data = [{"id": res["path"], "review_score":res["review_score"], "title": res["title"],
-             "content": res["content"]} for res in results]
+
+    data = [{"id": res["path"], "title": res["title"],
+             "content": res["content"], "sentiment":res["sentiment"], "length":len(res["content"])} for res in results]
 
     ngf = NgramFilter(minsize=0, maxsize=3)
     rext = RegexTokenizer()
     stream = rext(search.text)
 
     corrected = searcher.correct_query(query, search.text)
+
     if not len(results):
         query = parser.parse(corrected.string)
         results = searcher.search(query)
-
-        data = [{"id": res["path"], "review_score":res["review_score"], "title": res["title"],
-                 "content": res["content"], "sort": (res.score + float(res["review_score"]))} for res in results]
-
-        data.sort(key=lambda company: company["sort"],reverse=True)
+        data = [{"id": res["path"], "title": res["title"],
+                 "content": res["content"], "sentiment":res["sentiment"]} for res in results]
 
     ngrams = list(filter(None, [token.text for token in ngf(stream)]))
     print(ngrams)
