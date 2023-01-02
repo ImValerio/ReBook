b0fd9ed693185d3e3a8dfb36351aa2b6c897a188
Valerio <valerio.valletta@icloud.com>
2023-01-02 23:18:34 +0100

    testing dataset and sentiment analysis

--- main.py
+++ main.py
@@ -1,58 +1,45 @@
+from server.utils_fn import calculateSentimentNltk, calculateSentiment, removeStopWords
+import csv
+import os.path
 from whoosh.analysis import StemmingAnalyzer, NgramFilter, StandardAnalyzer, NgramTokenizer
 from whoosh.index import create_in
-from whoosh.fields import *
+from whoosh.fields import TEXT, ID, NUMERIC, Schema
 from decimal import Decimal
-import os.path
-import csv
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
-from torch.nn import functional as F
+from decimal import *
 
-from whoosh.qparser import QueryParser
+
 my_analyzer = StandardAnalyzer()
 
 schema = Schema(
     book_title=TEXT(analyzer=my_analyzer, stored=True),
     review_title=TEXT(analyzer=my_analyzer, stored=True),
     path=ID(stored=True, sortable=True),
-    content=TEXT(analyzer=my_analyzer, stored=True),
+    content=TEXT(analyzer=my_analyzer, stored=True, sortable=True),
     review_score=NUMERIC(stored=True),
     sentiment=NUMERIC(int, decimal_places=4, stored=True, sortable=True)
 )
-#schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True))
+# schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True))
 if not os.path.exists("indexdir"):
     os.mkdir("indexdir")
 ix = create_in("indexdir", schema)
 writer = ix.writer()
 
 
-tokenizer = AutoTokenizer.from_pretrained(
-    "juliensimon/reviews-sentiment-analysis")
-model = AutoModelForSequenceClassification.from_pretrained(
-    "juliensimon/reviews-sentiment-analysis")
-
-
-def calculateSentiment(review):
-    inputs = tokenizer(review, return_tensors="pt",
-                       truncation=True)
-
-    logits = model(**inputs).logits
-    softmax = F.softmax(logits, dim=1)
-    return round(softmax[0][1].item(), 4)
-
-
 with open('dataset/Books_rating.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        if line_count > 10000:
+        if line_count > 1000:
             break
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            # sentiment = Decimal(calculateSentiment(row[9]))
-            sentiment = 0
-            print(line_count, row[1])
+            filtred_content = removeStopWords(row[9])
+            sentiment = round(
+                Decimal(calculateSentiment(row[9])), 4)
+            # sentiment = Decimal(calculateSentiment(filtred_content))
+            print(line_count, sentiment)
             writer.add_document(
                 book_title=row[1], review_title=row[8], review_score=float(row[6]), path=row[0], content=row[9], sentiment=sentiment)
             line_count += 1
--- server/main.py
+++ server/main.py
@@ -13,12 +13,13 @@ from whoosh.query import *
 from models.BM25F import BM25F
 from models.CustomWeight import CustomWeight
 from whoosh.scoring import WeightScorer
+from utils_fn import calculateSentimentNltk
 
 app = FastAPI()
 
 
 def max_matcher():
-    return 1
+    return 100000
 
 
 class SearchText(BaseModel):
@@ -26,27 +27,14 @@ class SearchText(BaseModel):
     mode: Union[str, None] = None
     page: Union[int, None] = 1
 
-# TODO: aggiungere sentiment nella funzione di qui sotto
-
-
-def pos_score_fn(searcher, fieldname, text, matcher):
-    matcher.max_quality = max_matcher
-    poses = matcher.value_as("positions")
-    docnum = matcher.id()
-    colreader = searcher.reader().column_reader("sentiment")
-    sentiment = float(colreader[docnum])
-    position_score = 1/(poses[0] + 1)
-    percentage_score = position_score * sentiment
-
-    score = round(position_score + percentage_score, 5)
-
-    return score
-
 
 def pos_sentiment_fn(searcher, fieldname, text, matcher):
+
     docnum = matcher.id()
     colreader = searcher.reader().column_reader("sentiment")
     sentiment = float(colreader[docnum])
+    print("SENTIMENT: ", sentiment)
+
     return sentiment
 
 
@@ -55,14 +43,14 @@ def read_item(search: SearchText):
 
     ix = open_dir("../indexdir")
 
-    pos_weighting = scoring.FunctionWeighting(pos_score_fn)
-    # pos_weighting = CustomWeight()
+    # pos_weighting = scoring.FunctionWeighting(pos_score_fn)
+    pos_weighting = CustomWeight()
     # k1: importanza frequenza di un termine; b: importanza lunghezza del documento
     if search.mode == "CONTENT_TEXT":
         pos_weighting = BM25F(B=0.75, content_B=1.0, K1=1.5)
 
     elif search.mode == "CONTENT_SENTIMENT":
-        pos_weighting = scoring.FunctionWeighting(pos_sentiment_fn)
+        pos_weighting = CustomWeight(pos_sentiment_fn)
 
     from whoosh import query
     searcher = ix.searcher(weighting=pos_weighting)
@@ -95,23 +83,23 @@ def read_item(search: SearchText):
             dcg += (rel / math.log2(i + 1))
 
     data = [{"id": res["path"], "book_title":res["book_title"], "review_title": res["review_title"],
-             "content": res["content"], "sentiment":res["sentiment"], "length":len(res["content"]), "review_score":res["review_score"],  "score":res.score} for res in results]
+             "content": res["content"], "length":len(res["content"]), "review_score":res["review_score"],  "score":res.score, "sentiment":res["sentiment"]} for res in results]
 
-    # ngf = NgramFilter(minsize=0, maxsize=3)
-    # rext = RegexTokenizer()
-    # stream = rext(search.text)
+    ngf = NgramFilter(minsize=0, maxsize=3)
+    rext = RegexTokenizer()
+    stream = rext(search.text)
 
     corrected = searcher.correct_query(query, search.text)
 
-    # if not len(results):
-    #     query = parser.parse(corrected.string)
-    #     results = searcher.search(query)
-    #     data = [{"id": res["path"], "title": res["title"],
-    #              "content": res["content"], "sentiment":res["sentiment"]} for res in results]
+    if not len(results):
+        query = parser.parse(corrected.string)
+        results = searcher.search(query)
+        data = [{"id": res["path"], "title": res["title"],
+                 "content": res["content"], "sentiment":res["sentiment"]} for res in results]
 
-    # ngrams = list(filter(None, [token.text for token in ngf(stream)]))
-    # query = Or([Term("content", ngram) for ngram in ngrams])
+    ngrams = list(filter(None, [token.text for token in ngf(stream)]))
+    query = Or([Term("content", ngram) for ngram in ngrams])
 
-    # results = searcher.search(query)
+    results = searcher.search(query)
 
     return {"corrected": corrected.string, "results": data, "ngrams": list(results), "DCG": dcg}
